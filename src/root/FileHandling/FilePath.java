d8a6949acb54c144bdd9d30c7b4c5c50963f7669
StefanTieber <stefantieber23@gmail.com>
2020-02-20 17:33:40 +0100

    Change FilePath to math Hashcode

--- src/root/FileHandling/FilePath.java
+++ src/root/FileHandling/FilePath.java
@@ -1,31 +1,20 @@
 package root.FileHandling;
 
-class FilePath {
-    private static String DIRECTORY = "C:\\Users\\Steve\\Documents\\_Documents\\ccc\\ccc2019_2\\";
-    private static String RESOURCE_DIR = "resource\\";
+public class FilePath {
+    private static final String RESOURCE_PATH = "???\\src\\resources\\";
 
-    private static String LEVEL = "level1";
-    //private static String LEVEL = "level2";
-    //private static String LEVEL = "level3";
-    //private static String LEVEL = "level4";
-    //private static String LEVEL = "level5";
-    //private static String LEVEL = "level6";
-    //private static String LEVEL = "level7";
-    //private static String LEVEL = "level8";
+    private static final String FILE_NAME = "a_example";
+//    private static final String FILE_NAME = "b_small";
+//    private static final String FILE_NAME = "c_medium";
+//    private static final String FILE_NAME = "d_quite_big";
+//    private static final String FILE_NAME = "e_also_big";
 
-    private static String FILE_NUMBER = "1";
-    //private static String FILE_NUMBER = "2";
-    //private static String FILE_NUMBER = "3";
-    //private static String FILE_NUMBER = "4";
-    //private static String FILE_NUMBER = "5";
+    private static final String INPUT_FILE_EXTENSION = ".in";
+    private static final String OUTPUT_FILE_EXTENSION = ".out";
 
-    private static String RESOURCE_EXT = ".resource";
-    private static String INPUT_EXT = ".in";
-    private static String OUTPUT_EXT = ".out";
+    private static final String INPUT_RESOURCE_PATH = RESOURCE_PATH + "inputs\\";
+    private static final String OUTPUT_RESOURCE_PATH = RESOURCE_PATH + "outputs\\";
 
-    private static String FILE_PATH = LEVEL + "\\" + LEVEL + "_" + FILE_NUMBER;
-
-    static String RESOURCE_PATH = RESOURCE_DIR + LEVEL + "_" + FILE_NUMBER + RESOURCE_EXT;
-    static String INPUT_PATH = DIRECTORY + FILE_PATH + INPUT_EXT;
-    static String OUTPUT_PATH = DIRECTORY + FILE_PATH + OUTPUT_EXT;
+    public static final String INPUT_PATH = INPUT_RESOURCE_PATH + FILE_NAME + INPUT_FILE_EXTENSION;
+    public static final String OUTPUT_PATH = OUTPUT_RESOURCE_PATH + FILE_NAME + OUTPUT_FILE_EXTENSION;
 }
