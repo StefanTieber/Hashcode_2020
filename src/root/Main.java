eabc164324e7988556065647e81621edf388d9a0
Thomas Tieber <Thomas.Tieber@catalysts.cc>
2020-02-20 18:13:12 +0100

    test von thomas

--- src/root/Main.java
+++ src/root/Main.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("test commit");
+
         List<String> strings = FileReader.readStrings();
 
         FileWriter.writeStrings(strings);
