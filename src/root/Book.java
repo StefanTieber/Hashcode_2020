5896d2a3e20a7458b1f01c8d6ec8cc793e120654
StefanTieber <stefantieber23@gmail.com>
2020-02-20 19:13:41 +0100

    FileReader

--- src/root/Book.java
+++ src/root/Book.java
@@ -3,4 +3,9 @@ package root;
 public class Book {
     public Integer id;
     public Integer score;
+
+    public Book(Integer id, Integer score) {
+        this.id = id;
+        this.score = score;
+    }
 }
--- src/root/FileHandling/FileReader.java
+++ src/root/FileHandling/FileReader.java
@@ -1,5 +1,7 @@
 package root.FileHandling;
 
+import root.Book;
+import root.Library;
 import root.Matrix;
 
 import java.io.BufferedReader;
@@ -7,83 +9,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
-    public static List<String> readStrings() {
+    public static InputDTO readInputFile() {
         String filePath = FilePath.INPUT_PATH;
 
-        List<String> strings = new ArrayList<>();
+        InputDTO inputDTO = new InputDTO();
 
         try {
             BufferedReader reader = new BufferedReader(new java.io.FileReader(filePath));
 
-            String line = reader.readLine();
-            String[] parts = line.split("\\s+");
-            int numberOfRows = Integer.parseInt(parts[0]);
+            String line;
+            String[] parts;
 
-            for (int i = 0; i < numberOfRows; i++) {
+            line = reader.readLine();
+            parts = line.split("\\s+");
+            inputDTO.numberBooks = Integer.parseInt(parts[0]);
+            inputDTO.numberLibraries = Integer.parseInt(parts[1]);
+            inputDTO.numberDays = Integer.parseInt(parts[2]);
+
+            line = reader.readLine();
+            parts = line.split("\\s+");
+
+            for (int i = 0; i < inputDTO.numberBooks; i++) {
+                int score = Integer.parseInt(parts[i]);
+                inputDTO.books.add(new Book(i, score));
+            }
+
+            for (int i = 0; i < inputDTO.numberLibraries; i++) {
+                Library library = new Library();
+
                 line = reader.readLine();
-                //System.out.println(line);
-                strings.add(line);
+                parts = line.split("\\s+");
+
+                library.numberBooks = Integer.parseInt(parts[0]);
+                library.timeForSignup = Integer.parseInt(parts[1]);
+                library.booksPerDay = Integer.parseInt(parts[2]);
+
+                line = reader.readLine();
+                parts = line.split("\\s+");
+
+                for (int j = 0; j < library.numberBooks; j++) {
+                    int id = Integer.parseInt(parts[j]);
+                    library.books.add(inputDTO.books.get(id));
+                }
+
+                inputDTO.libraries.add(library);
             }
 
             reader.close();
 
-            return strings;
+            return inputDTO;
         } catch (Exception e) {
             System.err.println("Exception occurred trying to read " + filePath);
             e.printStackTrace();
             return null;
         }
-    }
-
-    public static Matrix readMatrixFile() {
-        String filePath = FilePath.INPUT_PATH;
-
-        Matrix matrix = new Matrix();
-
-        try {
-            BufferedReader reader = new BufferedReader(new java.io.FileReader(filePath));
-
-            matrix = readMatrix(reader);
-
-            reader.close();
-        } catch (Exception e) {
-            System.err.println("Exception occurred trying to read " + filePath);
-            e.printStackTrace();
-        }
-
-        return matrix;
-    }
-
-    private static Matrix readMatrix(BufferedReader reader) {
-        Matrix matrix = new Matrix();
-
-        String line;
-        String[] parts;
-
-        try {
-            line = reader.readLine();
-            parts = line.split("\\s+");
-
-            matrix.y = Integer.parseInt(parts[0]);
-            matrix.x = Integer.parseInt(parts[1]);
-            matrix.values = new int[matrix.y][matrix.x];
-
-            for (int iY = 0; iY < matrix.y; iY++) {
-                line = reader.readLine();
-                parts = line.split("\\s+");
-
-                for (int iX = 0; iX < matrix.x; iX++) {
-                    matrix.values[iY][iX] = Integer.parseInt(parts[iX]);
-                }
-            }
-        } catch (NumberFormatException e) {
-            System.err.println("First line is not a number!");
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.err.println("Exception occurred while reading file!");
-            e.printStackTrace();
-        }
-
-        return matrix;
     }
 }
