1d2d5a44191b73c83314df699c6ce72c6edfaf9c
Thomas Tieber <Thomas.Tieber@catalysts.cc>
2020-02-20 19:18:16 +0100

    implement validator

--- src/root/Main.java
+++ src/root/Main.java
@@ -2,39 +2,28 @@ package root;
 
 import root.FileHandling.FileReader;
 import root.FileHandling.FileWriter;
+import root.FileHandling.InputDTO;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
-
-    static List<Library> example = new ArrayList<>();
-
+    public static int NUMBER_DAYS;
 
     public static void main(String[] args) {
-        Book b1 = new Book();
-        b1.id=1;
-        Book b2 = new Book();
-        b2.id=2;
-        Book b3 = new Book();
-        b3.id=3;
+        InputDTO input = FileReader.readInput();
+        NUMBER_DAYS = input.numberDays;
 
+        List<Library> libraries = findSolution(input.libraries);
 
+        FileWriter.writeStrings(libraries);
 
-        example.add(new Library());
+        Validator.validate(libraries);
+    }
 
+    public static List<Library> findSolution(List<Library> libraries) {
+        //sort by books per day
+        //sort books by score
 
-//        List<String> strings = FileReader.readStrings();
-//
-        FileWriter.writeStrings(example);
-
-//        Validator.validate(strings);
-
-
-
-
-
-
+        return libraries;
     }
 }
--- src/root/Validator.java
+++ src/root/Validator.java
@@ -1,10 +1,35 @@
 package root;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Validator {
-    public static void validate(List<String> strings) {
-        System.out.println(strings.size() + " lines found");
-        System.out.println("VALID SOLUTION");
+    public static void validate(List<Library> libraries) {
+        List<Book> books = new ArrayList<>();
+        int days = 0;
+
+        for (Library library : libraries) {
+            days += library.timeForSignup;
+
+            int daysActive = Main.NUMBER_DAYS - days;
+            int booksScanned = daysActive * library.booksPerDay;
+
+            books.addAll(library.books.subList(0, booksScanned));
+        }
+
+        Collection<Book> nonDuplicatedEmployees = books.stream()
+                .<Map<Integer, Book>>collect(HashMap::new, (m, e) -> m.put(e.id, e), Map::putAll)
+                .values();
+
+        int score = 0;
+
+        for (Book book : nonDuplicatedEmployees) {
+            score += book.score;
+        }
+
+        System.out.println("SCORE: " + score);
     }
 }
